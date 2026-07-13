780df6d48de036cf2c2f54ced3435d65de21569a
Niranjan <chauhanniranjan98@gmail.com>
2026-07-13 14:49:06 +0530

    Fix server log message

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 import { PORT } from "./solution.js";
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
